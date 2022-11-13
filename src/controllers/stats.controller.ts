6ca87c3eac5a5d5795f8da474e8d848f389c5b70
Joseph Rodman <josephrodman5@gmail.com>
2022-11-13 11:43:48 -0700

    initial commit

---
+++ src/controllers/stats.controller.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from 'express';
+import statsService from '@services/stats.service';
+
+class StatssController {
+  public statsService = new statsService();
+
+  public getStats = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const data: any[] = await this.statsService.getStats();
+
+      res.status(200).json({ data: data, message: 'getStats' });
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+export default StatssController;
---
+++ src/services/stats.service.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+class StatsService {
+
+  private async getRequest(url: string): Promise<any> {
+    const key = '92b33b3c766b4421923b11b00b62adc5';
+    const headers = { 'Ocp-Apim-Subscription-Key': key };
+    const options = { headers };
+
+    try {
+      const resp = await axios.get(url, options);
+      console.log(resp.data);
+      return resp.data;
+    } catch (err) {
+      console.log(err);
+      return 'Error';
+    }
+  }
+
+  public async getStats(): Promise<any> {
+    const url = 'https://api.sportsdata.io/v3/nfl/scores/json/CurrentSeason';
+
+    const data = this.getRequest(url);
+
+    return data;
+  }
+}
+
+export default StatsService;
