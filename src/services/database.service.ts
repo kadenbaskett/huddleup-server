72e6ce02f4a4847ddce91f69d9a3951fefa24a3f
Justin Perez <u1248103@umail.utah.edu>
2023-01-27 09:23:58 -0700

    added teams to the getUserLeagues endpoints on the front end so that I can use those on the front end

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -422,6 +422,17 @@ class DatabaseService {
                         },
                     },
                 },
+                include:{
+                    teams: {
+                        where: {
+                            managers: {
+                                some:{
+                                    user_id:userID,
+                                },
+                            },
+                        },
+                    },
+                },
             });
         }
         catch(e)
@@ -437,9 +448,9 @@ class DatabaseService {
         try {
             return await this.client.league.findMany({
                 where: {
-                    // settings: {
-                    //     public_view: true,
-                    // },
+                    settings: {
+                        public_join: true,
+                    },
                 },
                 include: {
                     teams: {
@@ -447,6 +458,13 @@ class DatabaseService {
                             rosters: true,
                         },
                     },
+                    settings:
+                    {
+                        include: {
+                            scoring_settings: true,
+                        },
+                    },
+                    
                 },
             });
         }
