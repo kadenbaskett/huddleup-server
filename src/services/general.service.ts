636c3e67a17581abebee304690ba450db2865df5
u1189630@eng.utah.edu <u1189630@eng.utah.edu>
2023-04-10 00:05:48 -0600

    t

--- src/services/general.service.ts
+++ src/services/general.service.ts
@@ -79,18 +79,15 @@ export function startDraftChildProcess(leagueId: number, port: number): void
     // uses nodemon if not production otherwise node dist/serv.js
     //NODE_ENV defaults to development
     const isProduction = process.env.NODE_ENV === 'production';
-    const script = isProduction ? 'dist/server.js' : leagueId.toString();
+    const script = (isProduction ? 'dist/server.js' : '') + ' ' + leagueId.toString() + ' ' + port.toString();
     
-    const args = isProduction
-      ? []
-      : [ leagueId.toString(), port.toString() ];
+    console.log('script: ', script);
     
     const child = spawn('cross-env', [
       `NODE_ENV=${process.env.NODE_ENV || 'development'}`,
       'SERVICE=websocket',
       isProduction ? 'node' : 'nodemon',
       script,
-      ...args,
     ], { shell: true });
       
     child.stdout.on('exit', (code, signal) => {
