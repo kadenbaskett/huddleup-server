bcb29561d28f8a0855f1d88a966f45ac89ea7c3b
Justin Perez <u1248103@umail.utah.edu>
2023-04-18 11:15:14 -0600

    fixed league names and team names again

--- src/datasink/seed.ts
+++ src/datasink/seed.ts
@@ -968,12 +968,12 @@ class Seed {
   }
 
   generateLeagueNames(numLeagues) {
-    const generate = require('project-name-generator');
+    const generate = require('sports-team-name-generator');
     const names = [];
     const rand = Math.round(Math.random() * 1000);
 
     for (let i = 0; i < numLeagues; i++) {
-      names.push(generate().spaced);
+      names.push(generate());
     }
 
     return names;
